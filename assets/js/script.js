ed213a086210680112ff59e48544a4fb1a626bb1
eden essame <eden_essame80@hotmail.com>
2024-02-08 13:39:05 +0000

    add empty functions

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+//main function
+function runGame(params) {
+    
+}
+
+function checkAnswer(params) {
+    
+}
+//perform calculation and return correct answer and game type to checkAnswer function
+function calculateCorrectAnswer(params) {
+    
+}
+// increment and log correct answers
+function incrementScore(params) {
+    
+}
+//increment and log wrong answers
+function incrementWrongAnswer(params) {
+    
+}
+// display the questions
+function displayAdditionQuestion(params) {
+    
+}
+
+function displaySubtractQuestion(params) {
+    
+}
+
+function displayMultiplyQuestion(params) {
+    
+}
+
+function displayDivideQuestion(params) {
+    
+}
