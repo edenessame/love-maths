cb345652ffc7684157990ba8b52a0d84e4aea761
eden essame <eden_essame80@hotmail.com>
2024-02-08 16:00:36 +0000

    add addition function

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,22 +15,33 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                //template literal in `` because it contains a variable: ${}
-                alert(`You clicked ${gameType}`)
+                //call runGame function
+                runGame(gameType);
             }
 
         })
     }
+    //default game to run on open
+    runGame("addition");
+
 })
 
 /**
  *  The main game "loop", called when the script is first loaded
  *  and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     //creates 2 random number between 1-25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        //throw statement stops game running and sends error message to console
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer(params) {
@@ -49,8 +60,11 @@ function incrementWrongAnswer(params) {
     
 }
 // display the questions
-function displayAdditionQuestion(params) {
-    
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
+
 }
 
 function displaySubtractQuestion(params) {
