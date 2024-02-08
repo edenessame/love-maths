3991313da027bb6e73c19a3c2f97840f57fa4dd7
eden essame <eden_essame80@hotmail.com>
2024-02-08 15:17:25 +0000

    add alerts when buttons are clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
+//wait for the DOM to finish loading before running the game
+//good idea to wait for DOM to load befor you start running code so you arent targeting elements that haven't loaded yet
+//get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    //get all the buttons in an array, there are 5 buttons- [0,1,2,3,4]
+    let buttons = document.getElementsByTagName("button");
+
+    //go through the buttons arrayand return each element and store each item in the variable buttons, dont have to use index notation to get elements in the array
+    for (let button of buttons) {
+        //button is now each button and when one is clicked the code inside the block will load
+        button.addEventListener("click", function() {
+            //shows an alert telling the user which button was clicked, either submit or which type of game was clicked
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                //template literal in `` because it contains a variable: ${}
+                alert(`You clicked ${gameType}`)
+            }
+
+        })
+    }
+})
+
 //main function
 function runGame(params) {
     
